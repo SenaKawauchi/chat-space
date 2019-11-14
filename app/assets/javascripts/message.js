a0a921157db6d426b0c462d9ba5c1788e7515a62
SenaKawauchi <sena.kawauchi@di-v.co.jp>
2019-11-14 19:24:15 +0900

    test
    test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,27 @@
 $(function(){
     function buildHTML(message){
-        var html = `<div class='message'>
-        <div class='upper-message'>
-        <div class='upper-message__user-name'>
-        ${message.user_name}
-        </div>
-        <div class='upper-message__date'>
-        ${message.create_at}
-        </div>
-        </div>
-        <div class='lower-message'>
-        <p class='lower-message__content'>
-        ${message.content}
-        </p>
-        
-        </div>
-        </div>`
+        var insertImage = '';
+        if (message.image) {
+          insertImage = `<img src="${message.image}">`;
+        }
+         var html = 
+         `<div class="message">
+            <div class="upper-message">
+            <div class="upper-message__user-name">
+         　　${message.user_name}
+         　</div>
+         <div class="upper-message__date">
+         ${message.created_at}
+         </div>
+         </div>
+         <div class="lower-message">
+         <p class="lower-message__content">
+         ${message.content}
+        　</p>
+         ${insertImage}
+         </div>
+         </div>`
+
         return html;
       }
 
@@ -24,8 +30,7 @@ $(function(){
         e.preventDefault();
         var formData = new FormData(this);
         var url = $(this).attr('action');
-        console.log(url);
-        console.log(formData);
+       
         $.ajax({
             url: url,
             type: "POST",
@@ -36,12 +41,19 @@ $(function(){
         })
         .done(function(data){
             var html = buildHTML(data);
-            $('.message').append(html);
-            $('.form__message').val('');
-            $('.form__message').prop('disabled', false);
+            if (data.length != 0) {
+            $('.messages').append(html);}
+            else {
+              alert('メッセージを入力してください');
+            }
+            
+            $('#new_message')[0].reset();
+            $('.form__message').animate({scrollTop: $('.form__message')[0].scrollHeight}, 10);
+            $('.form__submit').prop('disabled', false);
         })
+
         .fail(function(){
-            alert('error');
+            alert('メッセージ送信に失敗しました');
         })
     });
 });
